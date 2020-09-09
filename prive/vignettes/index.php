7390629d69da5c689ba0ee24e59e89bdade501ab
Cerdic <cedric@yterium.com>
2020-09-09 19:32:40 +0200

    Version vectorielle des vignettes de type de fichier puisque c'etait deja presque tout prepare par izo il y a des annees :) Les icones sont issues du repo https://git.spip.net/spip-contrib-graphismes/spip_tango/src/branch/master/tango_vignettes Il y a une planche contact qui permet de toutes les voir et notamment verifier qu'elles sont identiques aux PNG en allant sur l'URL plugins-dist/medias/prive/vignettes/ du site (On nettoiera les PNG avant la release)

--- prive/vignettes/index.php
+++ prive/vignettes/index.php
@@ -6,25 +6,53 @@
 </head>
 <body style="background: #fff; text-align: center;">
 <h1>Index des vignettes de SPIP</h1>
-<table>
-	<tr>
-		<th colspan='10'>Format png &amp; gif</th>
-		<?php
-		$myDir = opendir('.');
-		$i = 0;
-		while ($file = readdir($myDir)) {
-			if (preg_match(',\.(png|gif)$,i', $file)) {
-				$r = "\n\t<td style='text-align:center; padding:10px'>$file<br /><img src='$file' alt='$file' /></td>";
-				if ($i % 10) {
-					echo $r;
-				} else {
-					echo "</tr>\n<tr>", $r;
-				}
-				$i++;
-			}
-		}
-		?>
-	</tr>
-</table>
+<?php
+$files = array_merge(glob(__DIR__ . '/*.svg'),glob(__DIR__ . '/*.png'));
+sort($files);
+?>
+<h2><?=count($files) ?> Icones au Format SVG / PNG</h2>
+<div class="thumbnails">
+	<?php
+	foreach ($files as $file) {
+		$file = substr($file, strlen(__DIR__) +1);
+		$extension = substr($file,-3);
+		$r = "\n\t<figure class='ext-$extension'><img src='$file' alt='$file' /><figcaption>$file</figcaption></figure>";
+		echo $r;
+	}
+	?>
+</div>
+<style type="text/css">
+	.thumbnails {
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.thumbnails figure {
+		width: 6rem;
+		margin: 0.5rem;
+		padding: 0.5rem;
+		border: 1px solid #eee;
+		overflow: hidden;
+	}
+	.ext-png {
+		background: #888;
+		opacity: 0.5;
+	}
+	.thumbnails figcaption {
+		margin-top: 0.5rem;
+		font-weight: bold;
+	}
+	.thumbnails figure img {
+		width:52px;
+		max-width: 100%;
+		height: auto;
+	}
+	.thumbnails figure:hover,
+	.thumbnails figure:focus {
+		background: #f4f4f4;
+		overflow: visible;
+		opacity: 1;
+	}
+
+</style>
 </body>
 </html>
