573b38fdaf041157257f8f882e8fb9cf7e54c031
George <george@middleeastwatch.net>
2021-03-14 01:27:07 +0100

    [Salvatore] [source:lang/ medias] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ medias] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ medias] Mise a jour du bilan depuis https://trad.spip.net

--- lang/medias_ar.php
+++ lang/medias_ar.php
@@ -118,6 +118,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_illustrations' => 'الرسوم التوضيحية',
 	'info_image_max_poids' => 'يحب ان يكون حجم الصور أقل من @maxi@ (حجم هذا الملف يبلغ @actuel@).',
 	'info_image_max_taille' => 'يحب ان يكون حجم الصور أقل من @maxi@ (حجم هذا الملف يبلغ @actuel@).',
+	'info_image_min_taille' => 'يجب على الصور ان تكون @mini@ على الأقل (هذا الملف يبلغ @actuel@).',
 	'info_inclusion_directe' => 'ضم مباشر:',
 	'info_inclusion_vignette' => 'ضم المصغر:',
 	'info_installer_tous_documents' => 'تثبيت كل المستندات',
--- lang/medias_en.php
+++ lang/medias_en.php
@@ -117,6 +117,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_illustrations' => 'Illustrations',
 	'info_image_max_poids' => 'Images must necessarily be less than @maxi@ (this file is @actuel@).',
 	'info_image_max_taille' => 'Images must necessarily be less than @maxi@ (this file is @actuel@).',
+	'info_image_min_taille' => 'Images must be at least @mini@ (this file is @actuel@).',
 	'info_inclusion_directe' => 'Direct inclusion:',
 	'info_inclusion_vignette' => 'Include vignette:',
 	'info_installer_tous_documents' => 'Install all the documents',
